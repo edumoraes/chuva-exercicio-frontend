6337d25fb70a67ce7c25afbada10eed584e06348
Edu Moraes <edumoraesdg@gmail.com>
2022-04-11 19:57:14 -0400

    :sparkles: feat: Adicionando função 'criar tópico'

--- main.js
+++ main.js
@@ -22,6 +22,31 @@
     verMenos.classList.add('d-none');
     evento.preventDefault();
   });
+
+
+
+  const btnCriarTopico = document.querySelector('[data-btn="criar-topico"]');
+  const divCriarTopico = document.querySelector('[data-div="criar-topico"]');
+  const divConteudoCriarTopico = document.querySelector('[data-div="conteudo-criar-topico"]');
+
+  btnCriarTopico.addEventListener('click', (evento) => {
+    divConteudoCriarTopico.classList.add('d-none');
+    divCriarTopico.innerHTML = `
+    <span>Tem uma dúvida ou sugestão? Compartilhe seu feedback com os autores!</span>
+    <form>
+      <div class="mb-3">
+        <label for="" class="form-label">Assunto</label>
+        <input type="text" name="" id="" class="form-control" placeholder="Defina um tópico sucinto para notificar os autores..." aria-describedby="helpId">
+      </div>
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Conteúdo</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
+      <div class="mb-3">
+        <button type="submit" class="btn btn-primary mb-3">Enviar</button>
+      </div>
+    </form>`
+  });
   
 
 	
